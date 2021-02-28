b5598171d76ec4205398d4a16b97c754c83683a7
Maham-H <36160315+Maham-H@users.noreply.github.com>
2021-02-28 11:15:11 -0500

    Add files via upload

--- trainers.py
+++ trainers.py
@@ -7,10 +7,10 @@ from Networks import Large_NN,tiny_NN, section_NN
 import helperfunc as hf
 ### for each epoch returns loss of batch
 
-def train_epoch(NN,optimizer,criterion,perm,batch_size,X_train,y_train,i):
+def train_epoch(NN,optimizer,criterion,perm,batch_size,X_train,y_train,i,device):
     optimizer.zero_grad()
     indices = perm[i:i+batch_size]
-    batch_x, batch_y = X_train[indices], y_train[indices]
+    batch_x, batch_y = X_train[indices].to(device), y_train[indices].to(device)
 
     # in case you wanted a semi-full example
     outputs = NN.forward(batch_x)
@@ -22,21 +22,30 @@ def train_epoch(NN,optimizer,criterion,perm,batch_size,X_train,y_train,i):
 
 
 
-def trainsingle(config, X_train, y_train):
+def trainsingle(config, X_train, y_train,X_val,y_val):
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     N,D= np.shape(X_train)
     batch_size = config["batch_size"]
     iters = config["iters"]
     epochs = config["epochs"]
        
     NN = Large_NN(config)
+    NN.to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(NN.parameters(), lr=config["lr"], betas=(0.9, 0.99), 
                            eps=1e-08,weight_decay=0.0, amsgrad=False)
         
     X_train = torch.tensor(X_train, dtype=torch.float) #  tensor
-    Nb = N
-        
     y_train = torch.tensor(y_train, dtype=torch.float)#  tensor
+    X_val = torch.tensor(X_val, dtype=torch.float).to(device) #  tensor
+    y_val = torch.tensor(y_val, dtype=torch.float).to(device)#  tensor
+    
+    Nb = N
+    train_loss =[]
+    val_loss = []
+    
     try:
         Dy=y_train.size()[1]
     except:
@@ -45,41 +54,50 @@ def trainsingle(config, X_train, y_train):
     y_train= torch.reshape(y_train,(N,Dy))
 
     print("starting training")
-   
+    
     for k in range(int(epochs)):
         running_loss =0.0
         permutation = torch.randperm(X_train.size()[0])
 
         for j in range(iters):
             running_loss += train_epoch(NN,optimizer,criterion,
-                                        permutation,batch_size,X_train,y_train,k)
+                                        permutation,batch_size,X_train,y_train,k,device)
     
         if (k % config["display_interval"] == 0) and (config["print_it"]):
             print('iter : ',int(k),'\t train loss :'+"{:.3f}".format(running_loss/iters))#,
-
-    outputs = NN(X_train)
-    loss = criterion(outputs, y_train).item()
+            
+        train_loss.append(running_loss/iters)
+        outputs = NN(X_val)
+        val_loss.append(criterion(outputs, y_val).item())
+    outputs = NN(X_train.to(device))
+    loss = criterion(outputs, y_train.to(device)).item()
         
-    return NN, criterion,loss
+    return NN, criterion,loss,train_loss,val_loss
 
 
-def traintiny(config, X_train, y_train):
+def traintiny(config, X_train, y_train, X_val, y_val):
     N,D= np.shape(X_train)
     batch_size = config["batch_size"]
     iters = config["iters"]
     epochs =config["epochs"]
-        
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
     print("starting training")
         
     NN = tiny_NN(config)
+    NN.to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(NN.parameters(), lr=config["lr"], betas=(0.9, 0.99), 
                            eps=1e-08,weight_decay=0.0, amsgrad=False)
 
     X_train = torch.tensor(X_train, dtype=torch.float) #  tensor
+    y_train = torch.tensor(y_train, dtype=torch.float)#  tensor
+    X_val = torch.tensor(X_val, dtype=torch.float).to(device) #  tensor
+    y_val = torch.tensor(y_val, dtype=torch.float).to(device)#  tensor
+    
     Nb = N
-        
-    y_train = torch.tensor(y_train, dtype=torch.float)# 15k, tensor
+    train_loss =[]
+    val_loss = []
     try:
         Dy=y_train.size()[1]
     except:
@@ -93,18 +111,20 @@ def traintiny(config, X_train, y_train):
         permutation = torch.randperm(X_train.size()[0])
 
         for j in range(iters):
-            running_loss += hf.train_epoch(NN,optimizer,criterion,
-                                        permutation,batch_size,X_train,y_train,k)
+            running_loss += train_epoch(NN,optimizer,criterion,
+                                        permutation,batch_size,X_train,y_train,k,device)
     
         if (k % config["display_interval"] == 0) and (config["print_it"]):
             print('iter : ',int(k),'\t train loss :'+"{:.3f}".format(running_loss/iters))#,
-
-    outputs = NN(X_train)
-    loss = criterion(outputs, y_train).item()
+        train_loss.append(running_loss/iters)
+        outputs = NN(X_val)
+        val_loss.append(criterion(outputs, y_val).item())
         
-    return NN,criterion,loss
 
-def trainsect(config, In, out,Sn,o_n,bn):
+        
+    return NN,criterion,train_loss,val_loss
+
+def trainsect(config, In, out,Sn,o_n,bn, X_val, y_val):
         
     N,D= np.shape(In)
     c=config["c"]
@@ -114,26 +134,36 @@ def trainsect(config, In, out,Sn,o_n,bn):
     epochs = config["epochs"]
     criterion = nn.MSELoss()
     
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
     all_in = {}
     all_out = {}
+    val_in = {}
+    val_out = {}
+    train_loss ={}
+    val_loss = {}
     NN ={}
     loss=np.zeros(Sn)
     outputs = {}
     
     
     for i in range(Sn):
+        train_loss[str(i)] = []
+        val_loss[str(i)] = []
+        print("sorting data for section: ",i)
+        r = np.array(hf.filt(In[:,dim],o_n[i+1],bn[i],c))
+        all_in["batch_for_sec_"+str(i)]=In[r>=0.5]
+        all_out["batch_for_sec_"+str(i)]=out[r>=0.5]
+        
+        r = np.array(hf.filt(X_val[:,dim],o_n[i+1],bn[i],c))
+        val_in["batch_for_sec_"+str(i)]=X_val[r>=0.5]
+        val_out["batch_for_sec_"+str(i)]=y_val[r>=0.5]
+        #idx = np.where(a > 4)[0]
 
-        all_in["batch_for_sec_"+str(i)]=[]
-        all_out["batch_for_sec_"+str(i)]=[]
-        for n in range(N):
-            r = hf.filt(In[n,dim],o_n[i+1],bn[i],c)
-            if r >= 0.5:
-                all_in["batch_for_sec_"+str(i)].append(r*In[n])
-                all_out["batch_for_sec_"+str(i)].append(out[n])
-                
         print("starting training for section: ",i)
         
         NN[str(i)] = section_NN(config)
+        NN[str(i)].to(device)
         optimizer = optim.Adam(NN[str(i)].parameters(), lr=config["lr"], betas=(0.9, 0.99), 
                            eps=1e-08,weight_decay=0.0, amsgrad=False)
 
@@ -141,9 +171,16 @@ def trainsect(config, In, out,Sn,o_n,bn):
         y_train = all_out["batch_for_sec_"+str(i)]
         
         X_train = torch.tensor(X_train, dtype=torch.float) #  tensor
+        y_train = torch.tensor(y_train, dtype=torch.float)# tensor
+        
+        X_val1 = val_in["batch_for_sec_"+str(i)]
+        y_val1 = val_out["batch_for_sec_"+str(i)]
+        
+        X_val1 = torch.tensor(X_val1, dtype=torch.float).to(device) #  tensor
+        y_val1 = torch.tensor(y_val1, dtype=torch.float).to(device)# tensor
         Nb = X_train.size()[0]
         
-        y_train = torch.tensor(y_train, dtype=torch.float)# 15k, tensor
+        
         try:
             Dy=y_train.size()[1]
         except:
@@ -156,19 +193,23 @@ def trainsect(config, In, out,Sn,o_n,bn):
             permutation = torch.randperm(X_train.size()[0])
 
             for j in range(iters):
-                running_loss += hf.train_epoch(NN[str(i)],optimizer[str(i)],criterion[str(i)],
-                                            permutation,batch_size,X_train,y_train,k)
+                running_loss += train_epoch(NN[str(i)],optimizer,criterion,
+                                            permutation,batch_size,X_train,y_train,k,device)
     
-            if (k % config["display_interval"] == 0) and (config["print_it"]):    # print every display_interval=100 mini-batches
+            if (k % config["display_interval"] == 0) and (config["print_it"]): # print every display_interval=100 mini-batches
                 print('iter : ',int(k),'\t train loss :'+"{:.3f}".format(running_loss/iters))#,
+            train_loss[str(i)].append(running_loss/iters)
+            outputs = NN[str(i)](X_val1)
+            val_loss[str(i)].append(criterion(outputs, y_val1).item())
+            
         net = NN[str(i)]
-        outputs[str(i)] = net(X_train)
-        loss[i] = criterion(outputs[str(i)], y_train).item()
+        outputs[str(i)] = net(X_train.to(device))
+        loss[i] = criterion(outputs[str(i)], y_train.to(device)).item()
 
         
         print('data points for section:'+str(i)+' are: ',str(Nb))
 
-    return NN, criterion,all_in,all_out,loss
+    return NN, criterion,all_in,all_out,loss,train_loss,val_loss
 
 
 
